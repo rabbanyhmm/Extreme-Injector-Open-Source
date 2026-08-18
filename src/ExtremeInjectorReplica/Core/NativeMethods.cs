b66d3fa1c236f561355619817d541ca2c559bfd3
rbnyx <sumonreza130@gmail.com>
2026-08-18 09:51:06 +0600

    feat: Add low-level CONTEXT, UNICODE_STRING, and NTAPI definitions to NativeMethods

--- src/ExtremeInjectorReplica/Core/NativeMethods.cs
+++ src/ExtremeInjectorReplica/Core/NativeMethods.cs
@@ -17,6 +17,63 @@ namespace ExtremeInjector.Core
         public const uint THREAD_SUSPEND_RESUME = 0x0002;
         public const uint THREAD_GET_CONTEXT = 0x0008;
         public const uint THREAD_SET_CONTEXT = 0x0010;
+        public const uint THREAD_QUERY_INFORMATION = 0x0040;
+
+        public const uint CONTEXT_X86_CONTROL = 0x00010001;
+        public const uint CONTEXT_X86_INTEGER = 0x00010002;
+        public const uint CONTEXT_X86_ALL = 0x0001003F;
+
+        public const uint CONTEXT_AMD64_CONTROL = 0x00100001;
+        public const uint CONTEXT_AMD64_INTEGER = 0x00100002;
+        public const uint CONTEXT_AMD64_ALL = 0x0010001F;
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct THREADENTRY32
+        {
+            public uint dwSize;
+            public uint cntUsage;
+            public uint th32ThreadID;
+            public uint th32OwnerProcessID;
+            public int tpBasePri;
+            public int tpDeltaPri;
+            public uint dwFlags;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct UNICODE_STRING
+        {
+            public ushort Length;
+            public ushort MaximumLength;
+            public IntPtr Buffer;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct CONTEXT_X86
+        {
+            public uint ContextFlags;
+            public uint Dr0, Dr1, Dr2, Dr3, Dr6, Dr7;
+            public uint FloatSave;
+            public uint SegGs, SegFs, SegEs, SegDs;
+            public uint Edi, Esi, Ebx, Edx, Ecx, Eax;
+            public uint Ebp, Eip, SegCs, EFlags, Esp, SegSs;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 512)]
+            public byte[] ExtendedRegisters;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 16)]
+        public struct CONTEXT_X64
+        {
+            public ulong P1Home, P2Home, P3Home, P4Home, P5Home, P6Home;
+            public uint ContextFlags;
+            public uint MxCsr;
+            public ushort SegCs, SegDs, SegEs, SegFs, SegGs, SegSs;
+            public uint EFlags;
+            public ulong Dr0, Dr1, Dr2, Dr3, Dr6, Dr7;
+            public ulong Rax, Rcx, Rdx, Rbx, Rsp, Rbp, Rsi, Rdi;
+            public ulong R8, R9, R10, R11, R12, R13, R14, R15;
+            public ulong Rip;
+            // Additional registers omitted for brevity
+        }
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
@@ -64,6 +121,21 @@ namespace ExtremeInjector.Core
         public static extern uint ResumeThread(IntPtr hThread);
 
         [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool GetThreadContext(IntPtr hThread, ref CONTEXT_X86 lpContext);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetThreadContext(IntPtr hThread, ref CONTEXT_X86 lpContext);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool CreateToolhelp32Snapshot(uint dwFlags, uint th32ProcessID);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool Thread32First(IntPtr hSnapshot, ref THREADENTRY32 lpte);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool Thread32Next(IntPtr hSnapshot, ref THREADENTRY32 lpte);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
         public static extern bool IsWow64Process(IntPtr hProcess, out bool wow64Process);
 
         // NTAPI definitions for LdrLoadDll and NtCreateThreadEx
@@ -81,5 +153,8 @@ namespace ExtremeInjector.Core
             uint sizeOfStackReserve,
             IntPtr bytesBuffer
         );
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        public static extern void RtlInitUnicodeString(ref UNICODE_STRING DestinationString, [MarshalAs(UnmanagedType.LPWStr)] string SourceString);
     }
 }
